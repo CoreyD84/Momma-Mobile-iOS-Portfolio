b71469d2ce8be2fa767ab02562525ed25772a1d3
CoreyD84 <corey84@email.phoenix.edu>
2026-02-18 02:20:42 +0000

    Update from https://github.com/CoreyD84/Codexia2/commit/f97b13eaa5d69207c4936a9bd09f7db4f159b1dc

--- latest-build/RootNavigator.swift
+++ latest-build/RootNavigator.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct RootNavigator: View {
     var body: some View {
         NavigationView {
-            Text("No Screens Loaded")
+            FinancialDashboardScreen()
         }
     }
 }
