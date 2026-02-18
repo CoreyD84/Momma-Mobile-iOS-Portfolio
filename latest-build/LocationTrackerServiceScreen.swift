2ad44b1c121763169d8046ad3a6372f43d67f19d
CoreyD84 <corey84@email.phoenix.edu>
2026-02-18 05:31:23 +0000

    Update from https://github.com/CoreyD84/Codexia2/commit/6625fff639618cb460f72a9a1ddd930471012d47

--- latest-build/LocationTrackerServiceScreen.swift
+++ latest-build/LocationTrackerServiceScreen.swift
@@ -6,6 +6,7 @@ struct LocationTrackerServiceScreen: View {
     var body: some View {
         
         VStack {
+            Text("MOMMA MOBILE LIVE").font(.caption).foregroundColor(.red) // 🚨 FORCE CHANGE
             Image(systemName: "checkmark.shield.fill")
                 .font(.system(size: 60))
                 .foregroundColor(.blue)
--- latest-build/MascotMoodFragmentScreen.swift
+++ latest-build/MascotMoodFragmentScreen.swift
@@ -6,6 +6,7 @@ struct MascotMoodFragmentScreen: View {
     var body: some View {
         
         VStack {
+            Text("MOMMA MOBILE LIVE").font(.caption).foregroundColor(.red) // 🚨 FORCE CHANGE
             Image(systemName: "checkmark.shield.fill")
                 .font(.system(size: 60))
                 .foregroundColor(.blue)
