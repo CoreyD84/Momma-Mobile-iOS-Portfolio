96155e2f229e4edcbb81f2cf5274fb50e02b5f27
CoreyD84 <corey84@email.phoenix.edu>
2026-02-18 04:37:01 +0000

    Update from https://github.com/CoreyD84/Codexia2/commit/e6a235d8becb424a903924ec07353e428c5aa20d

--- latest-build/SetupScreen.swift
+++ latest-build/SetupScreen.swift
@@ -1,23 +1,20 @@
 import SwiftUI
 
 struct SetupScreen: View {
-    @StateObject private var viewModel = SetupViewModel()
-
+    @EnvironmentObject var container: CodexiaContainer
+    
     var body: some View {
-        NavigationView {
-            VStack {
-                Image(systemName: "shield.checkered")
-                    .resizable()
-                    .frame(width: 80, height: 80)
-                    .foregroundColor(.blue)
-                    .padding()
-                Text("Setup Module Verified")
-                    .font(.headline)
-                Text("Architecture: MVI + Dependency Injection")
-                    .font(.caption)
-                    .foregroundColor(.gray)
-            }
-            .navigationTitle("Setup")
+        
+        VStack {
+            Image(systemName: "checkmark.shield.fill")
+                .font(.system(size: 60))
+                .foregroundColor(.blue)
+            Text("Setup Logic Active")
+                .font(.headline)
+            Text("Architecture: MVI + Dependency Injection")
+                .font(.caption)
+                .foregroundColor(.gray)
         }
+        .navigationTitle("Setup")
     }
 }
